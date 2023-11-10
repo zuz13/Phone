bad445384c202b8b31e47d1604b02c5905999d97
zuz13 <johanencarnacion2016@gmail.com>
2023-11-10 17:31:42 -0400

    img app

--- javaScript/app.js
+++ javaScript/app.js
@@ -114,7 +114,7 @@ instagram.addEventListener('click', ()=>{
                     <p>nasa</p>
                 </div>
 
-                <div class="containerP">
+                <div class="containerP" id=""storyOfF1>
                     <div class="state" class="pointer"></div>
                     <p>f1</p>
                 </div>
